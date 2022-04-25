72cc5c425aeb7c23fb572f901e7d006fdbcddf9b
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2022-04-25 10:30:57 -0700

    Lab 11 work

--- server.js
+++ server.js
@@ -3,27 +3,35 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
-const Book = require('./model/books');
+const Book = require('./models/books');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_CONNECTION);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', _ => {
+  console.log('We are connected to Mongo Atlas!');
+});
+
 app.get('/test', (request, response) => {
   response.send('test request received')
 })
-
-app.get('/books',getBooks);
+app.get('/books', getBooks);
 
 async function getBooks(request, response) {
-  try{
-      const books = await Book.find({}); 
-      response.status(200).send(books);
+  try {
+    console.log('hitting the sever');
+    const books = await Book.find({});
+    response.status(200).send(books);
   } catch (error) {
     console.error(error);
-    response.status(500).send(`An Error Occured in the Server... ${error.message}`);
+    response.status(500).send(`My bad! an error occurred in the server! Someone call the developer...${error.message}`);
   };
 };
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
