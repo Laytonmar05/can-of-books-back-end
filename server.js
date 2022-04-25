eab1c06d3c3dc81541051cc0a124595d8dc99e9e
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2022-04-25 10:17:03 -0700

    Lab 11 work

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const Book = require('./model/books');
+
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
-
   response.send('test request received')
-
 })
 
+app.get('/books',getBooks);
+
+async function getBooks(request, response) {
+  try{
+      const books = await Book.find({}); 
+      response.status(200).send(books);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send(`An Error Occured in the Server... ${error.message}`);
+  };
+};
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
