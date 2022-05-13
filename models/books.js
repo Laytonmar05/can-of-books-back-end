81e0c9a199ee04cbdc6da9aa32ab057f82d051c0
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2022-05-13 10:00:14 -0700

    Lab12 work

--- models/books.js
+++ models/books.js
@@ -1,6 +1,3 @@
-// Build a Mongoose 'Book' schema with valid keys for `title`, `description`, and `status`. 
-// Modularize your code by putting your schema and model in its own separate file and requiring the schema into your server.
-
 'use strict';
 
 const mongoose = require('mongoose');
@@ -11,7 +8,6 @@ const bookSchema = new mongoose.Schema({
   status: {type: String, uppercase: true, enum: ['LIFE-CHANGING', 'FAVORITE FIVE', 'RECOMMENDED TO ME']},
 });
 
-// Use your schema to craft a Book model.
 const bookModel = mongoose.model('book', bookSchema);
 
 module.exports = bookModel;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const Book = require('./models/books');
 
 const app = express();
 app.use(cors());
+app.use(express.json()); 
 
 const PORT = process.env.PORT || 3001;
 
@@ -23,6 +24,8 @@ app.get('/test', (request, response) => {
 })
 
 app.get('/books', getBooks);
+app.post('/books', createBook);
+app.delete('/books/:id', deleteBook);
 
 async function getBooks(request, response) {
   try {
@@ -34,5 +37,31 @@ async function getBooks(request, response) {
     response.status(500).send(`My bad! an error occurred in the server! Someone call the developer...${error.message}`);
   };
 };
+
+async function createBook(request, response) {
+  try {
+    const book = await Book.create(request.body);
+    response.status(201).send(book);
+
+
+  } catch (error) {
+    console.error(error);
+    response.status(500).send(`My bad! an error occurred in the server! Someone call the developer...${error.message}`);
+  }
+}
+
+async function deleteBook(request, response) {
+  try {
+    const id = request.params.id;
+    console.log('request.params.id = ', id);
+    
+    await Book.findByIdAndDelete(id);
+    console.log("deleted book!")
+    response.status(204);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send(`My bad! an error occurred in the server! Someone call the developer...${error.message}`);
+  }
+} 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
